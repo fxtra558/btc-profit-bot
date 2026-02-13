d744e33fdd6709cecad25a0a942e1a09aa2527fe
fxtra558 <fxtra558@gmail.com>
2026-02-12 20:17:13 -0500

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,10 @@
+# logger.py
+
+def log_trade(signal, entry, stop, target, size):
+    print("------------")
+    print(f"Signal: {signal}")
+    print(f"Entry: {entry}")
+    print(f"Stop: {stop}")
+    print(f"Target: {target}")
+    print(f"Position Size: {size}")
+    print("------------")
