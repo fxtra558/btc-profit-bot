c4722cc6baa0a453a540376d31f474a6c006c041
fxtra558 <fxtra558@gmail.com>
2026-02-13 20:18:35 -0500

    Update risk.py

--- risk.py
+++ risk.py
@@ -1,22 +1,7 @@
-# risk.py
-
-from config import RISK_PER_TRADE, DAILY_LOSS_LIMIT, STARTING_BALANCE
-
 class RiskManager:
-    def __init__(self):
-        self.balance = STARTING_BALANCE
-        self.daily_loss = 0
+    def __init__(self, wallet_balance=1000):
+        self.balance = wallet_balance
+        self.max_risk = 0.01  # 1%
 
-    def calculate_position_size(self, entry_price, stop_price):
-        risk_amount = self.balance * RISK_PER_TRADE
-        risk_per_unit = abs(entry_price - stop_price)
-        position_size = risk_amount / risk_per_unit
-        return position_size
-
-    def update_balance(self, profit_loss):
-        self.balance += profit_loss
-        if profit_loss < 0:
-            self.daily_loss += abs(profit_loss)
-
-    def hit_daily_limit(self):
-        return self.daily_loss >= self.balance * DAILY_LOSS_LIMIT
+    def calculate_size(self, price):
+        return self.balance * self.max_risk / price
